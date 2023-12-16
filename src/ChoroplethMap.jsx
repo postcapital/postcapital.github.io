4816ca9ed2d4be637f7148187734fc727182c3e7
James Carter <postcapital@gmail.com>
2023-12-16 18:07:55 +0000

    updated rotation

--- src/ChoroplethMap.jsx
+++ src/ChoroplethMap.jsx
@@ -1,11 +1,19 @@
 import { select, color, geoCircle, json, geoGraticule10 } from "d3";
-import { geoProjection, geoEqualEarth, geoOrthographic,
+import { geoProjection, geoOrthographic,
   geoNaturalEarth1, geoPath } from 'd3-geo'
 import { geoAugustRaw } from 'd3-geo-projection'
 import { mesh, feature } from 'topojson'
+import { easeCubicInOut } from 'd3-ease'
 import { useEffect, useRef } from 'preact/hooks'
 import { Component } from 'react'
 
+
+function getHeight() {
+  const [[x0, y0], [x1, y1]] = geoPath(projection.fitWidth(width, sphere)).bounds(sphere);
+  const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
+  projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
+  return dy;
+};
 
 class ChoroplethMap extends Component {
   constructor( props) {
@@ -13,8 +21,8 @@ class ChoroplethMap extends Component {
     super();
     
     this.state = { time: Date.now(), dataCon: props,
-    data: [], isLoading: true,  
-    projection: geoOrthographic(), features: null }
+    data: [], isLoading: true, rotate: [4,0], 
+    projection: null, features: null }
     console.log("ChoroplethMap instantiated with " + props)
   }
 
@@ -25,6 +33,7 @@ class ChoroplethMap extends Component {
 
       this.setState({
         data:  data,
+        projection: geoOrthographic(),
         isLoading: false
       })
     })
@@ -32,41 +41,78 @@ class ChoroplethMap extends Component {
 
       console.log('error: fetch failed'+error)
     })
+
+    setInterval(this.update(), 100)
+  }
+
+  static interpolateProjection(raw0, raw1) {
+    const {scale: scale0, translate: translate0} = ChoroplethMap.fit(raw0);
+    const {scale: scale1, translate: translate1} = ChoroplethMap.fit(raw1);
+    return t => geoProjection((x, y) => ChoroplethMap.lerp2(raw0(x, y), raw1(x, y), t))
+      .scale(ChoroplethMap.lerp1(scale0, scale1, t))
+      .translate(lerp2(translate0, translate1, t))
+      .precisiop(0.1)
+  }
+
+  animate (state) {
+    setTimeout(this.update(state), 100)
+  }
+
+  update (state) {
+    if(this.state.isLoading == true) return
+
+    const ease = easeCubicInOut
+    // if (r0 === r1) return
+    // const interpolate = ChoroplethMap.interpolateProjection(r0, r1)
+    // const setState = this.setState
+      //const t = Math.min(1,  ease(j / m))
+      // const p = geoNaturalEarth1().rotate([performance.now() / 100, 0])
+      //console.log('update:' + t)
+
+      const t = Date.now()   
+      const vx = 0.001, vy = -0.001 
+      const rotate = [vx * t, vy * t]
+      console.log('setting rotate:' + rotate)
+      //const interval = setInterval(function() {
+   // method to be executed;
+      //}, 50);
+
+      this.setState({rotate: rotate})
+
+  }
+  
+  static lerp1(x0, x1, t) {
+    return (1 - t) * x0 + t * x1;
+  }
+
+  static lerp2([x0, y0], [x1, y1], t) {
+    return [(1 - t) * x0 + t * x1, (1 - t) * y0 + t * y1];
+  }
+
+  static fit(raw) {
+    const width = 600 
+    const height = 800
+    const outline = {type: "Sphere"}
+    const p = geoProjection(raw).fitExtent([[0.5, 0.5], [width - 0.5, height - 0.5]], outline);
+    return {scale: p.scale(), translate: p.translate()};
   }
 
 
-
+  
   render(state) {
-
-    const animate = () => {
-      this.setState({ projection: geoEqualEarth()})
-      console.log('equialEarth started')
-    };
-    const width = 960
-    const height = 500
-
-    // const svg = select("#svgRef")
     
     var featuresPaths = null
     var path = null
     if(this.state.isLoading == false) {
 
       const countries = feature(this.state.data, this.state.data.objects.countries)
-      // features = countries.features
-      // const svgRef = useRef()
-      // const svg = select(svgRef.current)
-      /*svg.append('path')
-        .attr('class', 'sphere')
-        .attr('d', pathGenerator())
-        .style("fill", color("#edf5f9"))
-     */ 
        
-      const pathGenerator = geoPath().projection(this.state.projection)
-      /* svg.selectAll('path').data(countries.features)
-        .enter().append('path')
-        .attr('d', pathGenerator)
-        .style("fill", color("#d1cfcf"))*/
-      // path = countries.features
+      const width = 600 
+      const height = 800
+      const outline = {type: "Sphere"}
+      const pathGenerator = geoPath().projection(this.state.projection
+        //.fitExtent([[0.5, 0.5], [width - 0.5, height - 0.5]], outline)
+        .rotate(this.state.rotate))
       featuresPaths  = countries.features.map((f) => {
         return (
           <path d={pathGenerator(f)} fill="#c1d4dd" />
@@ -74,25 +120,26 @@ class ChoroplethMap extends Component {
       })
     }
     return (
-      <div class="resource" width={width}>
+      <div class="resource">
+        
       { state.isLoading &&
         <img src="../ontheroad_icon.png">On the Road logo</img>
       } 
       { !state.isLoading &&
-        <svg width="100%" height="500">
-          <g>{featuresPaths}</g>
-        </svg>
+        <div>
+          <button onClick={(state) => this.update(state)}>update</button>
+          <button onClick={(state) => this.animate(state)}>animate</button>
+          <svg width="100%" height="600">
+            <g>{featuresPaths}</g>
+          </svg>
+        </div>
       }
-        <button onClick={animate}>Animate</button>
-          
       </div>
     )
   }
 }
 
 export default ChoroplethMap
-
-
 
 function render2(projection) {
   //const path = geoPath(projection, svg);
@@ -105,88 +152,8 @@ function render2(projection) {
   //svg.beginPath(), path(outline), context.strokeStyle = "#000", context.stroke();
 }
 
-//var previousProjection = geoEquirectangularRaw;
 
-function update()  {
-  const r0 = previousProjection;
-  const r1 = projection;
-  if (r0 === r1) return;
-  previousProjection = r1;
-  const interpolate = interpolateProjection(r0, r1);
-  for (let j = 1, m = 45; true; ++j) {
-    const t = Math.min(1, ease(j / m));
-    render(interpolate(t).rotate([performance.now() / 100, 0]));
-    //yield;
-  }
-}
-
-// update();
 
 const sphere = ({ type: "Sphere" });
 
-function getHeight() {
-  const [[x0, y0], [x1, y1]] = geoPath(projection.fitWidth(width, sphere)).bounds(sphere);
-  const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
-  projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
-  return dy;
-};
 
-
-function sun() {
-  const now = new Date;
-  const day = new Date(+now).setUTCHours(0, 0, 0, 0);
-  const t = solar.century(now);
-  const longitude = (day - now) / 864e5 * 360 - 180;
-  return [longitude - solar.equationOfTime(t) / 4, solar.declination(t)];
-}
-
-const night =
-  geoCircle()
-    .radius(90)
-    .center(antipode(sun))
-    ();
-
-
-function interpolateProjection(raw0, raw1) {
-  const {scale: scale0, translate: translate0} = fit(raw0);
-  const {scale: scale1, translate: translate1} = fit(raw1);
-  return t => geoProjection((x, y) => lerp2(raw0(x, y), raw1(x, y), t))
-    .scale(lerp1(scale0, scale1, t))
-    .translate(lerp2(translate0, translate1, t))
-    .precision(0.1);
-}
-
-function lerp1(x0, x1, t) {
-  return (1 - t) * x0 + t * x1;
-}
-
-function lerp2([x0, y0], [x1, y1], t) {
-  return [(1 - t) * x0 + t * x1, (1 - t) * y0 + t * y1];
-}
-
-function fit(raw) {
-  const p = geoProjection(raw).fitExtent([[0.5, 0.5], [width - 0.5, height - 0.5]], outline);
-  return {scale: p.scale(), translate: p.translate()};
-}
-
-// ease = easeCubicInOut
-
-function antipode(longitude, latitude) { return [longitude + 180, -latitude]; }
-
-function update2() {
- d3.select('svg')
-   .selectAll('circle')
-   .data([data])
-   .join('circle')
-   .attr('cy', 50)
-   .transition()
-   .attr('cx', function(d) {
-     return d.x;
-   })
-   .transition()
-   .duration(750)
-   .ease(d3.easeBounce)
-   .attr('r', function(d) {
-     return d.r;
-   });
-}
