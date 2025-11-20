705227477823490914ea9ac70b9a0b0e1b7e6950
James Carter <postcapital@gmail.com>
2025-11-20 15:53:45 +0000

    updating lazy load data to cities and routes

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ class PostcapitalMap extends Component {
 
         this.state = {
             time: Date.now(),
-            data: null, isLoading: true, rotate: [0, 0],
-            links: [],
+
+            data: [], isLandLoading: true,
+            isLinksLoading: true,
+            isCitiesLoading: true, rotate: [0, 0],
+            links: [], cities: [],
             projection: geoConicEquidistant(), features: null
         }
         console.log("Map instantiated");
@@ -30,6 +33,7 @@ class PostcapitalMap extends Component {
             ).then((data) => {
                 this.setState({
                     data: data,
+                    isLandLoading: false
                     //projection: geoOrthographic(),
                     //isLoading: true
                 })
@@ -38,12 +42,12 @@ class PostcapitalMap extends Component {
 
                 console.log('error: fetch failed' + error)
             });
-        fetch("data/routes_2024.json").then((response) => response.json())
+        fetch("data/routes-oag-2024.json").then((response) => response.json())
             .then((data) => {
 
                 this.setState({
                     links: data,
-                    isLoading: false,
+                    isRoutesLoading: false,
                 })
             }).catch((error) => {
 
@@ -51,44 +55,31 @@ class PostcapitalMap extends Component {
                 console.log('error: fetch failed' + error);
             });
 
+        fetch("data/cities_5e5.json").then((response) => response.json())
+            .then((data) => {
+
+                this.setState({
+                    cities: data,
+                    isCitiesLoading: false,
+                })
+            }).catch((error) => {
+
+
+                console.log('error: fetch failed' + error);
+            });
         // setInterval(this.update(), 100)
     }
 
     /*static interpolateProjection(raw0, raw1) {
-      const {scale: scale0, translate: translate0} = ChoroplethMap.fit(raw0);
-      const {scale: scale1, translate: translate1} = ChoroplethMap.fit(raw1);
-      return t => geoProjection((x, y) => ChoroplethMap.lerp2(raw0(x, y), raw1(x, y), t))
-        .scale(ChoroplethMap.lerp1(scale0, scale1, t))
+      const {scale: scale0, translate: translate0} = PostcapitalMap.fit(raw0);
+      const {scale: scale1, translate: translate1} = PostcapitalMap.fit(raw1);
+      return t => geoProjection((x, y) => PostcapitalMap.lerp2(raw0(x, y), raw1(x, y), t))
+        .scale(PostcapitalMap.lerp1(scale0, scale1, t))
         .translate(lerp2(translate0, translate1, t))
         .precision(0.1)
     }*/
 
-    animate(state) {
-        setTimeout(this.update(state), 100)
-    }
 
-    /*update(state) {
-        if (this.state.isLoading == true) return
-
-        // const ease = easeCubicInOut
-        // if (r0 === r1) return
-        // const interpolate = ChoroplethMap.interpolateProjection(r0, r1)
-        // const setState = this.setState
-        //const t = Math.min(1,  ease(j / m))
-        // const p = geoNaturalEarth1().rotate([performance.now() / 100, 0])
-        //console.log('update:' + t)
-
-        const t = Date.now()
-        const vx = 0.001, vy = -0.001
-        const rotate = [vx * t, vy * t]
-        // console.log('setting rotate:' + rotate)
-        //const interval = setInterval(function() {
-        // method to be executed;
-        //}, 50);
-
-        this.setState({ rotate: rotate })
-
-    }*/
 
     static lerp1(x0, x1, t) {
         return (1 - t) * x0 + t * x1;
@@ -98,13 +89,6 @@ class PostcapitalMap extends Component {
         return [(1 - t) * x0 + t * x1, (1 - t) * y0 + t * y1];
     }
 
-    static fit(raw) {
-        const width = 600
-        const height = 800
-        const outline = { type: "Sphere" }
-        const p = geoProjection(raw).fitExtent([[0.5, 0.5], [width - 0.5, height - 0.5]], outline);
-        return { scale: p.scale(), translate: p.translate() };
-    }
 
     handleMouseDown = (e) => {
         //alert("touch down");
@@ -113,8 +97,7 @@ class PostcapitalMap extends Component {
     }
 
     handleMouseUp = (e) => {
-        //alert(Object.keys(e).toString());
-        this.setState({ rotate: [e.touches[0].pageX, 0] })
+        this.setState({ rotate: [e.touches[0].pageX, e.touches[0].pageY] })
         //console.log("handleMouseUp:" + e.pageY)
     }
 
@@ -126,26 +109,37 @@ class PostcapitalMap extends Component {
 
     render(state) {
 
-        const width = window.screen.width;
-        var featuresPaths = null
-        var path = null
-        var circle = geoCircle().center([-4.42, 55.84]).radius(1);
-        if (this.state.isLoading != true && this.state.data != null) {
+        // var featuresPaths = null
+        // var path = null
+        // var circle = geoCircle().center([-4.42, 55.84]).radius(1);
+        if (!this.state.isLandLoading) {
             const countries = feature(this.state.data, this.state.data.objects.land)
-            const height = window.screen.height;
-            const outline = { type: "Sphere" }
+            //const outline = { type: "Sphere" }
+            
+                //.fitExtent([[0.7, 0.7], [this.state.widt - 0.7, this.state.height - 0.7]], outline)
             const pathGenerator = geoPath().projection(this.state.projection
-                .fitExtent([[0.7, 0.7], [width - 0.7, height - 0.7]], outline)
-                .rotate(this.state.rotate)
-                .scale(width / 5))
+                .rotate(this.state.rotate));
+                //fitExtent([0.7,0.7], [window.screen.width, window.screen.height/2]));
+                //.scale(this.state.width / 10));
             var featuresPaths = countries.features.map((f) => {
                 const countryName = f.name;
+
                 return h('path', {
                     d: pathGenerator(f),
                     fill: "#c5d3d8", tooltip: countryName
                 });
             });
-            var populi = h('path', { d: pathGenerator(circle()), fill: "pink" });
+            var populi = this.state.cities.map((c) => {
+
+                var circle = geoCircle().center([c.longitude, c.latitude]).radius(c.population / 2e6)();
+                return h('path', {
+                    d: pathGenerator(circle),
+                    stroke: "orange",
+                    fill: "none",
+                    "stroke-opacity": "30%"
+                }
+                );
+            })
 
             var routesPaths = this.state.links.map((l) => {
                 return h('path', {
@@ -156,7 +150,8 @@ class PostcapitalMap extends Component {
                     ),
                     fill: "none", stroke: "skyblue",
                     "stroke-linecap": "round",
-                    "stroke-width": l.seats_2024 / 2e5, tooltip: "route"
+                    "stroke-opacity": "25%",
+                    "stroke-width": l.seats_2024 / 4e5, tooltip: "route"
                 });
             });
             return h('div',
@@ -166,7 +161,7 @@ class PostcapitalMap extends Component {
                     onTouchMove: this.handleMouseUp,
                     onTouchEnd: this.handleMouseUp
                 }
-                , h('svg', { width: width, height: height }, h('g',
+                , h('svg', { width: window.screen.width, height: window.screen.width/2 }, h('g',
                     null, featuresPaths.concat(routesPaths).concat(populi))
                     //h('g', { style: "stroke-width:8; fill: none"}, routesPaths)
                 ));
